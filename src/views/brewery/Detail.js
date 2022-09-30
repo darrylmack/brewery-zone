756808aff4ec854730d4391793915290b7712c0b
Darryl Mack <darryl@mackmedia.io>
2022-09-30 16:29:25 -0700

    Base Detail layout

--- src/views/brewery/Detail.js
+++ src/views/brewery/Detail.js
@@ -1,14 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function BreweryDetail() {
+  const [brewery, setBrewery] = useState("");
+  const baseURL = "https://api.openbrewerydb.org/breweries";
+  const { id } = useParams();
+
+  const fetchBrewery = async () => {
+    const data = await fetch(`${baseURL}/${id}`).then((response) =>
+      response.json()
+    );
+    setBrewery(data);
+  };
+  useEffect(() => {
+    fetchBrewery();
+  }, []);
+
+  const { name, city, state, postal_code, country, phone, website_url } =
+    brewery;
+
+  if (!brewery) return <div>Loading...</div>;
+
   return (
-    <main>
-      <h1>Brewery Name</h1>
-      <p>Brewtown, Oregon 12345</p>
-      <p>United States</p>
-      <p>8005551234</p>
+    <main className="container w-full mx-auto pt-12 justify-center pb-32">
+      <h1 className=" font-bold text-3xl mb-1">{name}</h1>
       <p>
-        <a href="https://example.com" target="_blank" rel="noreferrer">
+        {city}, {state}
+      </p>
+      <p>{postal_code}</p>
+      <p className="mb-2">{country}</p>
+      <p>{phone}</p>
+      <p className="mb-3 font-semibold text-blue-600 hover:text-blue-800 cursor-pointer">
+        <a href={website_url} target="_blank" rel="noreferrer">
           View Website
         </a>
       </p>
